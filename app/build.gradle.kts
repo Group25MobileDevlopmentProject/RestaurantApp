99772f3a5233530c2668c258688bf1e6d46425f2
Cúán Tilson <cuantilson@gmail.com>
2025-03-21 14:17:58 +0200

    feat: Add Extended Material Icons and Activity Compose Dependencies
    - Added `androidx.material.icons.extended.v160` dependency for extended Material icons.
    - Added `androidx.activity.compose` dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core)
+    implementation (libs.androidx.material.icons.extended.v160)
+    implementation (libs.androidx.activity.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
